5d39f4259b45659c6b74a8460337e49b0a6ff844
xxxamfa <xxxamfa@gmail.com>
2021-06-19 18:23:40 +0800

    有問題要問

--- src/App.js
+++ src/App.js
@@ -8,12 +8,57 @@ import defaultFiles from "./utils/defaultFiles";
 import BottomBtn from "./components/BottomBtn";
 import { faFileImport, faPlus } from "@fortawesome/free-solid-svg-icons";
 import TabList from "./components/TabList";
+import { useState } from "react";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 
 function App() {
+  const [files, setFiles] = useState(defaultFiles);
+  const [activeFileID, setActiveFileID] = useState("");
+  const [openedFileIDs, setOpenedFileIDs] = useState([]);
+  const [unsavedFileIDs, setUnsavedFileIDs] = useState([]);
+  const openedFiles = openedFileIDs.map((openID) => {
+    return files.find((file) => file.id === openID);
+  });
+  const fileClick = (fileID) => {
+    setActiveFileID(fileID);
+    if (!openedFileIDs.includes(fileID)) {
+      setOpenedFileIDs([...openedFileIDs, fileID]);
+    }
+  };
+  const activeFile = files.find((file) => file.id === activeFileID);
+
+  const tabClick = (fileID) => {
+    setActiveFileID(fileID);
+  };
+
+  const tabClose = (id) => {
+    const tabsWithout = openedFileIDs.filter((fileID) => fileID !== id);
+    setOpenedFileIDs(tabsWithout);
+    if (tabsWithout.length > 0) {
+      setActiveFileID(tabsWithout[0]);
+    } else {
+      setActiveFileID("");
+    }
+  };
+
+  const fileChange = (id, value) => {
+    const newFiles = files.map((file) => {
+      if (file.id === id) {
+        file.body = value;
+      }
+      return file;
+    });
+    setFiles(newFiles);
+    if (!unsavedFileIDs.includes(id)) {
+      setUnsavedFileIDs([...unsavedFileIDs, id]);
+    }
+  };
+
   return (
     <div className="App container-fluid px-0">
       <div className="row no-gutters">
-        <div className="col-3">
+        <div className="col-3 bg-light left-panel">
           <FileSearch
             title="我的雲文檔"
             onFileSearch={(value) => {
@@ -21,10 +66,8 @@ function App() {
             }}
           />
           <FileList
-            files={defaultFiles}
-            onFileClick={(id) => {
-              console.log(id);
-            }}
+            files={files}
+            onFileClick={fileClick}
             onFileDelete={(id) => {
               console.log("Delete", id);
             }}
@@ -33,7 +76,7 @@ function App() {
               console.log(newValue);
             }}
           />
-          <div className="row no-gutters">
+          <div className="row no-gutters button-group">
             <div className="col">
               <BottomBtn
                 text="新建"
@@ -51,17 +94,30 @@ function App() {
           </div>
         </div>
         <div className="col-9 right-panel">
-          <TabList
-            files={defaultFiles}
-            onTabClick={(id) => {
-              console.log(id);
-            }}
-            activeId="1"
-            onCloseTab={(id) => {
-              console.log("close", id);
-            }}
-            unsaveIds={["1", "2"]}
-          />
+          {!activeFile && (
+            <div className="start-page">选择或者创建新的 Markdown 文档</div>
+          )}
+          {activeFile && (
+            <>
+              <TabList
+                files={openedFiles}
+                activeId={activeFileID}
+                unsaveIds={unsavedFileIDs}
+                onTabClick={tabClick}
+                onCloseTab={tabClose}
+              />
+              <SimpleMDE
+                key={activeFile && activeFile.id}
+                value={activeFile && activeFile.body}
+                onChange={(value) => {
+                  fileChange(activeFile.id, value);
+                }}
+                options={{
+                  minHeight: "515px",
+                }}
+              />
+            </>
+          )}
         </div>
       </div>
     </div>
