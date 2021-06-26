15f8b25718b193bdf2570524dbcf693edec253ab
xxxamfa <xxxamfa@gmail.com>
2021-06-26 15:09:21 +0800

    純node.js fsAPI改用promise測試

--- src/utils/fileHelper.js
+++ src/utils/fileHelper.js
@@ -1,28 +1,34 @@
-const fs = require("fs");
+const fs = require("fs").promises;
 const path = require("path");
+// 如此封裝目的是為讓code更好管理
 const fileHelper = {
-  readFile: (path, cb) => {
-    fs.readFile(path, { encoding: "utf8" }, (err, data) => {
-      if (!err) {
-        cb(data);
-      }
-    });
+  readFile: (path) => {
+    return fs.readFile(path, { encoding: "utf8" });
   },
-  writeFile: (path, content, cb) => {
-    fs.writeFile(path, content, { encoding: "utf8" }, (err) => {
-      if (!err) {
-        cb();
-      }
-    });
+  writeFile: (path, content) => {
+    return fs.writeFile(path, content, { encoding: "utf8" });
+  },
+  renameFile: (path, newPath) => {
+    return fs.rename(path, newPath);
+  },
+  deleteFile: (path) => {
+    return fs.unlink(path);
   },
 };
 
 const testPath = path.join(__dirname, "helper.js");
 // 新檔案產生路徑
 const testWritePath = path.join(__dirname, "hello.md");
-fileHelper.readFile(testPath, (data) => {
-  console.log("data", data);
+const renamePath = path.join(__dirname, "rename.md");
+// fileHelper.readFile(testPath).then((data) => {
+//   console.log("data", data);
+// });
+// fileHelper.writeFile(testWritePath, "## hello world").then(() => {
+//   console.log("寫入成功");
+// });
+// fileHelper.renameFile(testWritePath, renamePath).then(() => {
+//   console.log("重新命名成功");
+// });
+fileHelper.deleteFile(renamePath).then(() => {
+  console.log(`${renamePath} 刪除成功`);
 });
-fileHelper.writeFile(testWritePath, '## hello world', () => {
-    console.log('寫入成功');
-})
